dff997d9908bd81e03a7b1beaf99fb042f507158
Edwardm54 <edward54.m2@gmail.com>
2026-04-29 16:31:30 -0400

    Mejoras palaba larga

--- Palabra_larga_pequena.py
+++ Palabra_larga_pequena.py
@@ -1,27 +1,37 @@
-# 🎯 Enunciado
+# El usuario escribe una frase y tú:
 
-# El usuario escribe una frase:
-
-# Debes hacer TODO en un solo for:
-
-# Mostrar cada palabra
-# Decir si es larga (>5 letras)
-# Contar cuántas son largas
+# Muestras todas
+# Separas largas y cortas
+# Cuentas cada grupo
+# Muestras la más larga
 
 frase = input("Frase: ")
-palabras = frase.split()
-contador = 0
+palabras = frase.replace(",", "").split()
 largas = []
+cortas = []
+contador_largas = 0
+contador_cortas = 0
 
 for palabra in palabras:
-    if len(palabra) > 5:
-        print(f"\nTu palabra es larga: {palabra}")
-        largas.append(palabra)
-        contador += 1
-    else:
-        print(f"\nPalabra pequeña: {palabras}")
+    print(f"\nPalabra: {palabra}")
 
-print(f"Cantidad: {contador}")
+    if len(palabra) > 5:
+        print(f"Palabra larga: {palabra}")
+        largas.append(palabra)
+        contador_largas += 1
+
+    else:
+        print(f"Palabra corta: {palabra}")
+        cortas.append(palabra)
+        contador_cortas += 1
+        
+print("\n--- RESULTADOS ---")
+
+print(f"Cantidad largas: {contador_largas}")
+print(f"Cantidad cortas: {contador_cortas}")
 
 if largas:
-    print(f"Palabras mas larga: {max(largas, key=len)}")
+    print(f"Palabra mas larga: {max(largas, key=len)}")
+
+else:
+    print("No hay palabras largas")
