d7f1879690e90998260ecec9f1db78db55708b4d
Edwardm54 <edward54.m2@gmail.com>
2026-07-03 23:16:48 -0400

    Optimizado

--- SistemaInvi.py
+++ SistemaInvi.py
@@ -1,81 +1,61 @@
-import json
 print("----bienvenido al sistema de control de inventario----".upper()) #Bienvenida
 
-inventario = [] #Inventario general 
+inventario_productos = {}
 
 while True:
 
-    print("\nmenu de opciones".upper()) #Menu de opciones
+    print("====Menu de Opciones====")
     print("1. Registrar Producto")
-    print("2. Visualizar inventario")
-    print("3. Eliminar Producto")
+    print("2. Eliminar Producto")
+    print("3. Visualizar Inventario")
     print("4. Salir")
 
-    opcion = input("Elige una opcion: ") #Le pedimos al usuario que ingres una opcion valida
+    opcion = input("Que desea consultar?: ")
 
-    if opcion == "1": #Empezamos con las condiciones junto con Try y Except para evitar posibles errores
-        nombre_producto = input("Ingresa el nombre del producto: ")
-        try: #Usamos Try para evitar errores de letras.
-            cantidad = int(input("Cantidad a Ingresar: "))
-            precio_unitario = float(input("Precio Unitario: ")) 
+    if opcion == "1":
+
+        nombre_producto = input("Nombre del producto: ").lower().strip()
+        try:
+            cantidad = int(input("Cantidad: "))
+            precio_unitario = float(input("Precio Unitario: "))
         except ValueError:
-            print("Solo numeros...")
+            print("Solo numeros.")
             continue
+        precio_total = cantidad * precio_unitario
 
-        datos_compra = { #Guardamos toda la information dentro del dict
-        "nombre_producto" : nombre_producto,
-        "cantidad" : cantidad,
-        "precio_unitario" : precio_unitario,
-        "total" : cantidad * precio_unitario
-    }
-        inventario.append(datos_compra)
-        print("Producto registrado con éxito!")
+        inventario_productos[nombre_producto] = {
+            "cantidad" : cantidad,
+            "precio_unitario" : precio_unitario,
+            "precio_total" : precio_total
+        }
 
     elif opcion == "2":
-        if inventario == []: #Si inventario vacio no mostramos nada, solo el print
-            print("Inventario vacio...")
+        if inventario_productos == {}:
+            print("El inventario está vacío. No hay productos para eliminar. 🚫")
         else:
-            for elemento in inventario: #Con for mostraremos los productos ingresados en el dict
-                 print("-" * 30)
-                 print(f"Nombre del producto: {elemento['nombre_producto']}".title().strip())
-                 print(f"Cantidad ingresada: {elemento['cantidad']}")
-                 print(f"Precio unitario: {elemento['precio_unitario']:.2f}$")
-                 print(f"Precio total: {elemento['total']:.2f}$")
-                 print("-" * 30)
-                 print(json.dumps(inventario, indent=3, ensure_ascii=False))
-
+            eliminar_producto = input("Que producto desea eliminar?: ").lower().strip()
+            if eliminar_producto in inventario_productos:
+                del inventario_productos[eliminar_producto]
+                print("Producto eliminado con éxito.")
+            else:
+                print("El producto no se encuentra en el inventario. ❌")
+                
     elif opcion == "3":
-        if inventario == []:
-            print("Inventario vacio...")
-            continue
-        else: #Al tener un producto a eliminar saltamos a este else
-            print("Productos disponibles: ")
-            for elemento in inventario:
-                print(f"Producto: {elemento['nombre_producto']}")
-            eliminar = input("Ingresa el nombre del producto a eliminar: ").lower()
-        
-            if eliminar == "":
-                print("No se ingreso un producto...")
-                continue
-            encontrado = False
-            for elemento in inventario:
-                if elemento['nombre_producto'].lower() == eliminar:
-                    inventario.remove(elemento)
-                    print(f"Producto '{eliminar}' eliminado con exito!")
-                    encontrado = True
-                    break
+        if inventario_productos == {}:
+            print("El inventario está vacío. No hay productos para mostrar. 🚫")
+        for nombre, detalles in inventario_productos.items():
+            print(f"Nombre Del Producto: {nombre}")
+            print(f"Cantidad: {detalles['cantidad']}")
+            print(f"Precio Unitario: {detalles['precio_unitario']}")
+            print(f"Total: ${detalles['precio_total']} 📈")
 
-            if not encontrado:
-                print("Producto no encontrado en el inventario...")
-            
     elif opcion == "4":
-        salir = input("Salir? (s/n): ").lower()
-
+        salir = input("Desea salir? (s/n): ").lower()
         if salir == "s":
-            print("Cerrando el sistema....")
+            print("Saliendo del programa")
             break
         else:
-            print("Continuando...")
+            print("Continuando")
 
     else:
-        print("opcion no valida...❌")
+        print("Opcion no valida, intente nuevamente.")
